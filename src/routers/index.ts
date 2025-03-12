436d05c6360341a7410dd762e6e32d6a22ff8a95
Eastyn <1282861514@qq.com>
2025-03-12 23:05:25 +0800

    爱心雨伞相关的接口

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,12 +3,14 @@ import publicRoutes from './publicRouter'
 import authRoutes from './authRouter'
 import backboneRoutes from './backboneMembersRouter'
 import departmentsRoutes from './departmentsRouter'
+import umbrellaRoutes from './umbrellaRouter'
 
 const router = Router()
 
 router.use('/public', publicRoutes) // 开放API
 router.use('/auth', authRoutes) // 用户相关的API
 router.use('/backbone', backboneRoutes) // 骨干成员相关的API
-router.use('/departments', departmentsRoutes) // 骨干成员相关的API
+router.use('/departments', departmentsRoutes) // 部门相关的API
+router.use('/umbrella', umbrellaRoutes) // 爱心雨伞相关的API
 
 export default router;
